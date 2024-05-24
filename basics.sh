d6a8ba4b53df39f556cd7e5987beb06bc14088a0
PavelHolba <136717295+PavelHolba@users.noreply.github.com>
2024-05-24 12:25:14 +0200

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+# Exit immediately if any command fails
 set -e
 
-reset;clear;
+# Clear the terminal screen for clean output
+clear
 
+# Print a header indicating the start of the provisioning routine
 echo
-echo "## "
-echo "## routine: [provision-new-ubuntu-linux-server-basic] // state: [starting]"
-echo "## "
+echo "## Provisioning New Ubuntu Linux Server (Basic) ##"
 echo
 
-apt-get update
-apt-get upgrade -y
-apt-get dist-upgrade -y
+# Update package lists
+sudo apt-get update
 
-apt-get install -y net-tools
-apt-get install -y docker-compose
+# Perform distribution upgrade for handling changes in dependencies
+sudo apt-get dist-upgrade -y
+
+# Upgrade installed packages
+sudo apt-get upgrade -y
+
+# Install essential network utilities
+sudo apt-get install -y net-tools
 
 
+# Print a message indicating the completion of the provisioning routine
 echo
-echo "## "
-echo "## routine: [provision-new-ubuntu-linux-server-basic] // state: [completed]"
-echo "## "
+echo "## Provisioning Completed Successfully ##"
 echo
